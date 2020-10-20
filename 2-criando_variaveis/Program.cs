531c408df0f4c3511ec53cd54861831554beca0d
luizhsousa98 <luizhsousa26@gmail.com>
2020-10-19 21:35:38 -0300

    Calculadora C# simples

---
+++ 2-criando_variaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_criando_variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculadora calculadora = new Calculadora(10, 5);
+            Console.WriteLine(calculadora.Divisao());
+        }
+    }
+}
---
+++ 2-criando_variaveis/calculadora.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_criando_variaveis
+{
+    class Calculadora
+    {
+
+        private int a;
+        private int b;
+
+        public Calculadora(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int Soma()
+        {
+            return this.a + this.b;
+        }
+
+        public int Subtracao()
+        {
+            return this.a - this.b;
+        }
+
+        public int Multiplicacao()
+        {
+            return this.a * this.b;
+        }
+
+        public int Divisao()
+        {
+            return this.a / this.b;
+        }
+        
+    }
+}
